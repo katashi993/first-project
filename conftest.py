9fea86db902fa9105b65b429deff9e870d293069
katashi993 <155308023+katashi993@users.noreply.github.com>
2025-09-19 01:06:47 +0300

    3.6 PyTest — параметризация, конфигурирование, плагины

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: es, fr, ru, etc.")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+
+    # Настройки для Chrome
+    options = Options()
+    options.add_experimental_option('prefs', {
+        'intl.accept_languages': language,
+    })
+
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    browser.quit()
